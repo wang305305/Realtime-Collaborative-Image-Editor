c0dcd469e2e684947053d321858225dad64eeff0
wang305305 <wang305305@gmail.com>
2020-04-03 22:26:54 -0400

    added inputbox to enter hidden room

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,9 +4,16 @@
   socket.emit('getrooms');
   console.log("index js")
 
+  // enter a room
+
+  document.querySelector("#enter_room_form").addEventListener("submit", e => {
+    e.preventDefault();
+    let room_id = document.querySelector("#enter_room_input").value;
+    window.location.href="/room/"+room_id;
+  });
   // create a new private room.
   document.querySelector("#createPrivate").addEventListener("click", e => {
-    console.log("create private")
+    //console.log("create private")
     e.preventDefault();
     let room_name = document.querySelector("#room_name_input").value;
     let password = undefined;
@@ -16,36 +23,36 @@
     //let private_checkbox = document.querySelector("#private_option").checked;
     hidden = document.querySelector("#hidden_option").checked;
     password = document.querySelector("#password_input").value;
-    console.log("will emit "+ { room_name: room_name, password: password, hidden:hidden})
-    socket.emit("newroom", { room_name: room_name, password: password, hidden:hidden});
+    console.log("will emit " + { room_name: room_name, password: password, hidden: hidden })
+    socket.emit("newroom", { room_name: room_name, password: password, hidden: hidden });
   });
 
   // create a new public room.
   document.querySelector("#createPublic").addEventListener("click", e => {
-    console.log("create public")
+    //console.log("create public")
     e.preventDefault();
     let room_name = document.querySelector("#room_name_input").value;
     let password = undefined;
     let hidden = false;
     document.querySelector("#error_text").innerHTML = "";
     document.querySelector("#error_text").style.visibility = "hidden";
-    socket.emit("newroom", { room_name: room_name, password: password, hidden:hidden});
+    socket.emit("newroom", { room_name: room_name, password: password, hidden: hidden });
   });
-/*
-  document.querySelector("#private_option").addEventListener('change', function() {
-    if(this.checked) {
-      document.querySelector("#password_label").style.visibility = "visible";
-      document.querySelector("#password_input").style.visibility = "visible";
-      document.querySelector("#hidden_option").style.visibility = "visible";
-      document.querySelector("#hidden_label").style.visibility = "visible";
-    } else {
-      document.querySelector("#password_label").style.visibility = "hidden";
-      document.querySelector("#password_input").style.visibility = "hidden";
-      document.querySelector("#hidden_option").style.visibility = "hidden";
-      document.querySelector("#hidden_label").style.visibility = "hidden";
-    }
-  });
-*/
+  /*
+    document.querySelector("#private_option").addEventListener('change', function() {
+      if(this.checked) {
+        document.querySelector("#password_label").style.visibility = "visible";
+        document.querySelector("#password_input").style.visibility = "visible";
+        document.querySelector("#hidden_option").style.visibility = "visible";
+        document.querySelector("#hidden_label").style.visibility = "visible";
+      } else {
+        document.querySelector("#password_label").style.visibility = "hidden";
+        document.querySelector("#password_input").style.visibility = "hidden";
+        document.querySelector("#hidden_option").style.visibility = "hidden";
+        document.querySelector("#hidden_label").style.visibility = "hidden";
+      }
+    });
+  */
   // error.
   socket.on('error', error => {
     alert(error);
@@ -72,21 +79,12 @@
         li.innerHTML = `
         <a href="/room/${room.room_id}" class="room-link list-group-item">${room.room_name}</a>
         <button class="room-delete btn btn-secondary" title="delete this room">Delete</button>`;
-        li.querySelector('.room-delete').addEventListener('click', function(e){
-            socket.emit("deleteroom", { room_id: room.room_id });
-        }); 
+        li.querySelector('.room-delete').addEventListener('click', function (e) {
+          socket.emit("deleteroom", { room_id: room.room_id });
+        });
         room_list.append(li);
       });
-      document.getElementById("new-room2").classList.add("col-6")
-      document.getElementById("new-room2").style.marginRight = "5px";
-      document.getElementById("new-room2").style.paddingRight = "27px";
-      document.getElementById("new-room").classList.add("d-flex")
-      document.getElementById("new-room").classList.add("justify-content-between")
-    } else {
-      room_list_block.style.visibility = "hidden";
-      document.getElementById("new-room2").classList.remove("col-6")
-      document.getElementById("new-room").classList.remove("d-flex")
-      document.getElementById("new-room").classList.remove("justify-content-between")
+
     }
   });
 
