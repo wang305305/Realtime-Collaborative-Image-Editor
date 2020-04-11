0754e2ea45f5e1fe1290a596a639301fde4fa709
wang305305 <wang305305@gmail.com>
2020-04-10 23:07:54 -0400

    fixed all UI issues

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -7,9 +7,14 @@
   document.querySelector("#button-addon2").addEventListener("click", e => {
     e.preventDefault();
     let room_id = document.querySelector("#enter_room_input").value;
-    window.location.href = "/room/" + room_id;
+    socket.emit("enterroom", { room_id: room_id });
+    //window.location.href = "/room/" + room_id;
   });
 
+  // redirect
+  socket.on('redirect', data => {
+    window.location.href = data.destination;
+  });
   //delete a room
   document.querySelector("#delete-button").addEventListener("click", e => {
     e.preventDefault();
@@ -32,6 +37,15 @@
     socket.emit("newroom", { room_name: room_name, password: password, hidden: hidden });
   });
 
+  document.querySelector("#next_button").addEventListener("click", e => {
+    let room_name = document.querySelector("#room_name_input").value;
+    if (room_name == null || room_name == "") {
+      socket.emit("error", "please enter a room name");
+    } else {
+      window.$('#createRoomModal').modal();
+    }
+  });
+
   // create a new public room.
   document.querySelector("#createPublic").addEventListener("click", e => {
     e.preventDefault();
@@ -44,21 +58,34 @@
     socket.emit("newroom", { room_name: room_name, password: password, hidden: hidden });
   });
 
+  window.$('#createRoomModal').on('hidden.bs.modal', function (e) {
+    document.querySelector("#room_name_input").value = "";
+    document.querySelector("#room_name_input").focus();
+  });
+  window.$('#privateRoomModal').on('hidden.bs.modal', function (e) {
+    document.querySelector("#room_name_input").value = "";
+    document.querySelector("#room_name_input").focus();
+  });
+  window.$('#hiddenRoomModal').on('hidden.bs.modal', function (e) {
+    document.querySelector("#room_name_input").value = "";
+    document.querySelector("#room_name_input").focus();
+  });
+
   // write an error message to the screen.
   socket.on('error', message => {
     const error_text = document.querySelector("#error_text");
     error_text.style.visibility = "visible";
     error_text.innerHTML = message;
-    error_text.classList.add("elementToFadeInAndOut");
-    setTimeout(() => {
-      error_text.style.visibility = "hidden";
-    }, 5000);
   });
 
   // shows the new room id.
   socket.on('showID', id => {
+    document.querySelector("#hidden_room_id").innerHTML = id;
+    window.$('#hiddenRoomModal').modal();
+    /*
     document.querySelector("#hidden_room_id").style.visibility = "visible";
     document.querySelector("#hidden_room_id").innerHTML = id;
+    */
   });
 
   // display the list of rooms.
